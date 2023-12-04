12a8caac00e8feeaea42e184169bd459eea3b2ec
jscode <dbwlsthf90@korea.kr>
2023-12-05 00:18:58 +0900

    2장

---
+++ Desktop/projects/jwitter/src/firebase.js
@@ -0,0 +1,13 @@
+
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDiMNED-s90pjAldnucNG922zC0xD1L2yk",
+  authDomain: "jwitter-991a1.firebaseapp.com",
+  projectId: "jwitter-991a1",
+  storageBucket: "jwitter-991a1.appspot.com",
+  messagingSenderId: "761928522521",
+  appId: "1:761928522521:web:20834edf0094789bbb0388"
+};
+
+export default firebase.initializeApp(firebaseConfig);
