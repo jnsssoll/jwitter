9b3ed22611074a20c34c10ecee6018a48cd5735c
jscode <dbwlsthf90@korea.kr>
2023-12-29 16:36:02 +0900

    4장

--- Desktop/projects/jwitter/src/components/Router.js
+++ Desktop/projects/jwitter/src/components/Router.js
@@ -1,23 +1,27 @@
-import {useState} from "react";
-import {HashRouter as Router, Route, Routes} from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
-
-const AppRouter = () => {
-    const [isLoggedIn,setIsLoggedIn] = useState(true);
+import {HashRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
+const AppRouter = ({isLoggedIn}) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Routes>
                 {isLoggedIn ? (
+                 <>
                     <Route exact path="/" element={<Home />}>
-            
                     </Route>
+                    <Route exact path="/Profile" element={<Profile />}>
+                    </Route>
+                 </>
                 ) : (
                     <Route exact path="/" element={<Auth />}>
                         
                     </Route>
                 
                 )}
+                <Route path="*" element={<Navigate replace to="/" />}/>
             </Routes>
         </Router>
     );
--- Desktop/projects/jwitter/src/routes/Profile.js
+++ Desktop/projects/jwitter/src/routes/Profile.js
@@ -1,5 +1,13 @@
-function Profile() {
-    <span>profile</span>;
-}
+import { authService } from "fbase";
+
+const Profile = () => {
+    const onLogOutClick = () => authService.signOut();
+
+    return (
+        <>
+        <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    );
+};
 
 export default Profile;
